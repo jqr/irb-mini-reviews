d90a39429d8e898411e860252551c815602474c3
Elijah Miller <elijah.miller@gmail.com>
2013-11-13 20:52:07 -0500

    Whaaa.

--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -1,2 +1,11 @@
+require "active_support/all"
+
 class Phrase
+  def initialize(text)
+    @text = text
+  end
+
+  def word_count
+    Hash[@text.split(/\W+/).group_by(&:downcase).map { |k, v| [k, v.size] }]
+  end
 end
