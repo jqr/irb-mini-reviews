5e8f5e5ea4eaaca3e9d322016ee4e7f252fd6502
Elijah Miller <elijah.miller@gmail.com>
2013-11-13 19:46:09 -0500

    Nice.

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,2 +1,29 @@
 class Bob
+  def hey(message)
+    if    message.blank?    then "Fine. Be that way."
+    elsif message.all_caps? then "Woah, chill out!"
+    elsif message.question? then "Sure."
+    else                         "Whatever."
+    end
+  end
+end
+
+class NilClass
+  def blank?
+    true
+  end
+end
+
+class String
+  def blank?
+    self == ""
+  end
+
+  def question?
+    self[-1] == "?"
+  end
+
+  def all_caps?
+    upcase == self
+  end
 end
