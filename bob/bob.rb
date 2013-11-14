bf217a335443ac426e58291125faa3ec13393b8b
Elijah Miller <elijah.miller@gmail.com>
2013-11-13 19:46:51 -0500

    HASHES.

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,2 +1,35 @@
 class Bob
+  def initialize
+    @bob_proxy = Hash.new do |h, k|
+      if    k.blank?    then "Fine. Be that way."
+      elsif k.all_caps? then "Woah, chill out!"
+      elsif k.question? then "Sure."
+      else                         "Whatever."
+      end
+    end
+  end
+
+  def hey(message)
+    @bob_proxy[message]
+  end
+end
+
+class NilClass
+  def blank?
+    true
+  end
+end
+
+class String
+  def blank?
+    self == ""
+  end
+
+  def question?
+    self[-1] == "?"
+  end
+
+  def all_caps?
+    upcase == self
+  end
 end
