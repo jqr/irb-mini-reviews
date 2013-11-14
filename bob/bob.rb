22e51a9b3914e587393c22934e6f6549676b7a03
Elijah Miller <elijah.miller@gmail.com>
2013-11-13 19:50:59 -0500

    Super best #1 code.

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,15 +1,20 @@
 class Bob
   def initialize
+    # Bob proxy... he's no Bob, but he's pretty damn close.
     @bob_proxy = Hash.new do |h, k|
       if    k.blank?    then "Fine. Be that way."
       elsif k.all_caps? then "Woah, chill out!"
       elsif k.question? then "Sure."
-      else                         "Whatever."
+      else                   "Whatever."
       end
     end
   end
 
   def hey(message)
+    what_would_bob_say?(message)
+  end
+
+  def what_would_bob_say?(message)
     @bob_proxy[message]
   end
 end
